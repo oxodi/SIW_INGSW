5ea8fedcda99fd14914eacc727244b04d0a03f1b
aless <aless@160.97.145.116>
2018-12-14 17:50:57 +0100

    Aggiungo la classe Cliente

--- SIW/src/main/java/entita/Cliente.java
+++ SIW/src/main/java/entita/Cliente.java
@@ -1,7 +1,60 @@
 package entita;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Cliente {
-	public static void main(String[] args) {
-		System.out.println("ciao");
+	private int id;
+	private String nome;
+	private String cognome;
+	private Date dataDiNascita;
+	
+	public Cliente() {}
+	
+	public Cliente(int id, String nome, String cognome, Date dataDiNascita) {
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataDiNascita = dataDiNascita;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+	
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		return "Cliente["+ this.getId() + ","
+	}
+
+
 }
+
