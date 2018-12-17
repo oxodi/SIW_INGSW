4e668ae5d42ce7bbc3a13df417125627a056a993
aless <aless@160.97.140.251>
2018-12-17 18:39:14 +0100

    Alcune classi Dao

---
+++ SIW/src/main/java/persistence/dao/ClienteDao.java
@@ -0,0 +1,15 @@
+package persistence.dao;
+
+import java.util.List;
+
+import entita.Cliente;
+
+public interface ClienteDao {
+	public void salva(Cliente cliente);
+	public Cliente cercaPerChiavePrimaria(int id);
+	public List<Cliente> cercaTutti();
+	public void aggiorna(Cliente cliente);
+	public void cancella(Cliente cliente);
+	public void setPassword(Cliente cliente, String password);
+	
+}
