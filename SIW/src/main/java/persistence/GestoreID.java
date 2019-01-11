7af2128d4826ce44b68eeb4339f4797b57faaaa4
vinc_ <vinc_@DESKTOP-JM0IV29>
2019-01-11 11:27:02 +0100

    GestoreID

--- SIW/src/main/java/persistence/GestoreID.java
+++ SIW/src/main/java/persistence/GestoreID.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class GestoreID {
-	
+	//id automatico
 	private static final String query = "SELECT nextval('sequenza_id') AS id";
 
 	public static int getId(Connection connection) {
