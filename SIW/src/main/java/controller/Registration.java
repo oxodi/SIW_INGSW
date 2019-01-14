10b8404d6832eeb9175fc7443d294f5af3c4bbc8
Luca <Luca@DESKTOP-2KICHS1>
2019-01-14 12:43:29 +0100

    vuuyvgyvòy

--- SIW/src/main/java/controller/Registration.java
+++ SIW/src/main/java/controller/Registration.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,27 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 public class Registration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Registration() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	protected void doGet(HttpServletRequest req, 
+			HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getWriter().println("<h1>OK</h1>");
+		
+		RequestDispatcher rd = req.getRequestDispatcher
+				("registrazioneAzienda.html");
+		rd.include(req, resp);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	
 }
