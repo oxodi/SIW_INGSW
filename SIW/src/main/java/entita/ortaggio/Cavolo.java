d1d3a6e62f9bd25a10593d44943428296928bc27
aless <aless@160.97.138.179>
2019-01-14 18:28:01 +0100

    cosine

--- SIW/src/main/java/entita/ortaggio/Cavolo.java
+++ SIW/src/main/java/entita/ortaggio/Cavolo.java
@@ -1,14 +1,14 @@
 package entita.ortaggio;
 
 public class Cavolo extends Ortaggio {
-	
-	public Cavolo () {
+
+	public Cavolo() {
 		this.setId(2);
 		this.setNome("Cavolo");
 		this.setResa(1.0);
 	}
-	
-	public Cavolo(double prezzo, String tempoColtivazione, String periodoColtivazione,  int idAzienda) {
+
+	public Cavolo(double prezzo, String tempoColtivazione, String periodoColtivazione, int idAzienda) {
 		super(prezzo, tempoColtivazione, periodoColtivazione);
 		this.setId(2);
 		this.setNome("Cavolo");
--- SIW/src/main/java/entita/ortaggio/Cetriolo.java
+++ SIW/src/main/java/entita/ortaggio/Cetriolo.java
@@ -1,6 +1,6 @@
 package entita.ortaggio;
 
-public class Cetriolo extends Ortaggio{
+public class Cetriolo extends Ortaggio {
 
 	public Cetriolo() {
 		this.setId(3);
@@ -8,7 +8,7 @@ public class Cetriolo extends Ortaggio{
 		this.setResa(1.0);
 
 	}
-	
+
 	public Cetriolo(double prezzo, String tempoColtivazione, String periodoColtivazione) {
 		super(prezzo, tempoColtivazione, periodoColtivazione);
 		this.setId(3);
