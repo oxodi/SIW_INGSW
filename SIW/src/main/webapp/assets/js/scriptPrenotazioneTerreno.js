3799d1ac347d4135819b2b7514ea9091c579f9ce
vinc_ <vinc_@DESKTOP-JM0IV29>
2019-01-25 16:27:03 +0100

    update prenotazioneTerreno

--- SIW/src/main/webapp/assets/js/scriptPrenotazioneTerreno.js
+++ SIW/src/main/webapp/assets/js/scriptPrenotazioneTerreno.js
@@ -37,16 +37,20 @@ function changecolour() {
 
 	if(button == 1){
 		document.getElementById(button).style.background = "#cccccc";
+		document.getElementById(3).style.background = "#cccccc";
 		document.getElementById("completo").value = "true";
 		document.getElementById("parziale").value = "false";
 		document.getElementById(2).style.background = "#f7f3e8";
+		document.getElementById(4).style.background = "#f7f3e8";
 
 	}
 	if(button == 2){
 		document.getElementById(button).style.background = "#cccccc";
+		document.getElementById(4).style.background = "#cccccc";
 		document.getElementById("parziale").value = "true";	
 		document.getElementById("completo").value = "false";
 		document.getElementById(1).style.background = "#f7f3e8";
+		document.getElementById(3).style.background = "#f7f3e8";
 		
 
 	}
